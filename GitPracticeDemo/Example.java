f292e2c7f2b81cf5773f1db3e0965aaaa2fd8d97
manoharkonde <manohar.konde@gmail.com>
2016-07-16 13:40:42 +0530

    story #2324 : Added some code for testing.

---
+++ GitPracticeDemo/Example.java
@@ -0,0 +1,22 @@
+package Samples;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=10,b,c;
+    b=20;c=30;
+    final int money =100;
+    System.out.println( "addition of a,b is"+(a+b));
+    System.out.println(money);
+    System.out.println(c);
+    if(a<b){
+    	System.out.println("a is abig number");
+    }
+    else{
+    	System.out.println("b is a big number");
+    	System.out.println("added for test purpose");
+    }
+	}
+
+}
